79a8826a376e7b866300ffb32c08ec6e77857ee6
Mubashar Hussain <mubashar.cui@gmail.com>
2026-05-28 23:01:45 +0500

    sorted model loading folder name

--- msds24031_05_task5_simclr_evaluating.py
+++ msds24031_05_task5_simclr_evaluating.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    checkpoint = torch.load('results/simclr_model.pth', map_location =device)
+    checkpoint = torch.load('models/simclr_model.pth', map_location =device)
 
     encoder = resnet_encoder().to(device)
     projection_head = ProjectionHead().to(device)
